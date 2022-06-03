7081016798dcf76c9ed835bd28212ec21681ec69
Ivan Mochalov <ivam@tut.by>
2022-06-03 15:21:14 +0300

    Change the way of fill of List from sequence to random oder

--- src/ITOveroneSecond/HomeWork.java
+++ src/ITOveroneSecond/HomeWork.java
@@ -10,18 +10,19 @@ package ITOveroneSecond;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class HomeWork {
 
     public static void main(String[] args) {
         List<Integer> numbersList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            numbersList.add(i, i);
+        Random numGen = new Random();
+        for (int i = 0; i < 30; i++) {
+            numbersList.add(numGen.nextInt(130)-30);
         }
         System.out.println(numbersList);
         System.out.println("Min of numberList = " + findMin(numbersList));
     }
-
     static int findMin(List<Integer> someList) {
         return Collections.min(someList);
     }
